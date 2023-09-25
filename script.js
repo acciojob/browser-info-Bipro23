d4890f1b5a7da61b2699d3d76fd25df3afaeb172
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 18:59:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+// Get browser information from the navigator object
+        const browserName = navigator.appName;
+        const browserVersion = navigator.appVersion;
+
+        // Display the browser information in the specified format
+        const browserInfoMessage = "You are using " + browserName + " version " + browserVersion;
+
+        // Add the message to the "browser-info" div
+        const browserInfoDiv = document.getElementById("browser-info");
+        browserInfoDiv.textContent = browserInfoMessage;
